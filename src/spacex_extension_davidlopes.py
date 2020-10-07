86cd7f40d17f62d9582d9ef6fa1413b3b4650a01
dlopes7 <davidribeirolopes@gmail.com>
2020-10-07 16:13:36 +0000

    Make ship names unique

--- src/spacex_extension_davidlopes.py
+++ src/spacex_extension_davidlopes.py
@@ -15,14 +15,14 @@ class SpaceXExtension(RemoteBasePlugin):
         # Save the groups to a dictionary to be used later
         groups = {}
         for ship_type in ship_types:
-            groups[ship_type] = self.topology_builder.create_group(ship_type, ship_type)
+            groups[ship_type] = self.topology_builder.create_group(f"{ship_type} David Lopes", f"{ship_type} David Lopes")
 
         for ship in ships:
             # Grab the Group object for this ship
             ship_group = groups[ship["ship_type"]]
 
             # Create the custom device
-            custom_device = ship_group.create_device(ship["ship_id"], ship["ship_name"])
+            custom_device = ship_group.create_device(f"{ship['ship_id']} David Lopes", f"{ship['ship_name']} David Lopes")
 
             # Send a simple metric
             custom_device.absolute("fuel", ship["fuel"])
